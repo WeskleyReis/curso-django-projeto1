b143b1a30a6fa25a1256585f02f5431dd95124d8
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-29 19:31:24 -0300

    Atualiza a estrutura de URLs e remove templates não utilizados; adiciona novo template para a página inicial.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import home, contato, sobre
+from .views import home
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contato/', contato, name='contato'),
-    path('sobre/', sobre, name='sobre'),
 ]
