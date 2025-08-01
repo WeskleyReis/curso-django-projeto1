85f4fd3d5b2e828edc00c05ebdaf2bb5981d0713
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-01 17:23:00 -0300

    Adiciona estrutura de templates e configurações para o aplicativo 'recipes'; implementa páginas de listagem e detalhes de receitas.

--- projeto/settings.py
+++ projeto/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            BASE_DIR / 'base_templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'base_static',
 ]
 STATIC_ROOT = BASE_DIR / 'static'
 
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, recipe
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('recipes/<int:id>/', recipe, name='recipe'),
 ]
