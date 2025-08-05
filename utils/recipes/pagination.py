9177f66ea37114c3cc9b6ea7e7c095b7eb477082
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-05 14:45:26 -0300

    #

--- utils/recipes/pagination.py
+++ utils/recipes/pagination.py
@@ -6,9 +6,10 @@ def make_pagination_range(page_range, qty_pages, current_page):
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
     total_pages = len(page_range)
-    start_range_offset = abs(start_range) if start_range < 0 else 0
+    start_range_offset = 0
 
     if start_range < 0:
+        start_range_offset = abs(start_range)
         start_range = 0
         stop_range += start_range_offset
 
