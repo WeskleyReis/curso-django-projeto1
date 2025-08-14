1705cce9cb4ef86cc77cdae036c49df2bb44aca7
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-14 18:58:12 -0300

    Adiciona link para o perfil do autor na exibição da receita e otimiza a consulta para pré-carregar o perfil do autor.

--- recipes/views.py
+++ recipes/views.py
@@ -22,6 +22,7 @@ class RecipeListViewBase(ListView):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published=True)
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('author__profile')
         return qs
     
     def get_context_data(self, *args, **kwargs):
